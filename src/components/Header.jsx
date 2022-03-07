58e1d175348c0555292f3ffdef3277dcdfd60ee3
Stepanov Igor <stipyk1309@gmail.com>
2022-03-07 20:13:12 +0300

    lesson 7-12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" />
+    </header>
+  );
+};
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.planetware.com/wpimages/2019/11/canada-in-pictures-beautiful-places-to-photograph-morraine-lake.jpg" />
+      </div>
+      <div>ava + discription</div>
+      <div>
+        My post
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
