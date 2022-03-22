da2deeec0617de7ba52e02e22bf121282749929a
Stepanov Igor <stipyk1309@gmail.com>
2022-03-22 22:18:40 +0300

    lesson 30 REPEAT

--- src/App.js
+++ src/App.js
@@ -6,31 +6,32 @@ import Dialogs from "./components/Dialogs/dialogs";
 import News from "./components/News/news";
 import Music from "./components/Music/music";
 import Settings from "./components/Settings/settings";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { state } from "./redux/state";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/profile/*" element={<Profile state={state.profilePage} />} />
-            <Route
-              path="/dialogs/*"
-              element={
-                <Dialogs state={state.dialogsPage}  /> // props дата файлы из state.js
-              }
-            />
-            <Route path="/news/*" element={<News />} />
-            <Route path="/music/*" element={<Music />} />
-            <Route path="/settings/*" element={<Settings />} />
-          </Routes>
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Routes>
+          <Route
+            path="/profile/*"
+            element={<Profile state={state.profilePage} />}
+          />
+          <Route
+            path="/dialogs/*"
+            element={
+              <Dialogs state={state.dialogsPage} /> // props дата файлы из state.js
+            }
+          />
+          <Route path="/news/*" element={<News />} />
+          <Route path="/music/*" element={<Music />} />
+          <Route path="/settings/*" element={<Settings />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import s from "./Header.module.css";
 const Header = () => {
   return (
     <header className={s.header}>
-      <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo"/>
+      <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo" />
     </header>
   );
 };
