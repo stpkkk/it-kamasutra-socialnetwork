7c483c2abde4dae2b93933437702a4cb7791fc8a
Stepanov Igor <stipyk1309@gmail.com>
2022-04-01 20:59:00 +0300

    lesson 37 store, bind, OOP

--- src/index.js
+++ src/index.js
@@ -3,25 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import state, { subscribe } from "./redux/state";
-import {
-  addPost,
-  updateNewPostText,
-  addMessage,
-  updateNewMessageText,
-} from "./redux/state";
+import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
   ReactDOM.render(
     <BrowserRouter>
       <React.StrictMode>
         <App
-          state={state}
-          addPost={addPost}
-          updateNewPostText={updateNewPostText}
-          addMessage={addMessage}
-          updateNewMessageText={updateNewMessageText}
+          state={store.getState()}
+          addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+          addMessage={store.addMessage.bind(store)}
+          updateNewMessageText={store.updateNewMessageText.bind(store)}
         />
       </React.StrictMode>
     </BrowserRouter>,
@@ -29,9 +23,9 @@ let rerenderEntireTree = (state) => {
   );
 };
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/redux/store.js
@@ -0,0 +1,72 @@
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi! How are you", likesCount: "7" },
+        { id: 2, message: "It`s my first post", likesCount: "42" },
+      ],
+      newPostText: "Тяу Тяу Тяу",
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Masha" },
+        { id: 2, name: "Maxim" },
+        { id: 3, name: "Alexey" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Vlad" },
+        { id: 6, name: "Valera" },
+      ],
+      messages: [
+        { id: 1, message: "Hey" },
+        { id: 2, message: "Do you feel ok?" },
+        { id: 3, message: "How you doing?" },
+        { id: 4, message: "WTF" },
+        { id: 5, message: "How is your it-kamasutra" },
+        {
+          id: 6,
+          message:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam unde dolore perspiciatis voluptate non odit esse? Consequuntur doloremque animi corrupti at suscipit vitae, magnam officia unde facere, voluptatem, perferendis fuga.",
+        },
+      ],
+      newMessageText: "на пенек сел",
+    },
+  },
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log("State changed");
+  },// в прошлом rerenderEntireTree, теперь мы уведомляем подписчика
+  addPost() {
+    let newPost = {
+      id: 3,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = ""; //зануляем2
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  addMessage() {
+    let newMessage = {
+      id: 7,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messages.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newText) {
+    this._state.dialogsPage.newMessageText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  }, //observer наблюдатель
+};
+
+export default store;
