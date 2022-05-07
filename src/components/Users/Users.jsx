6096d1a90215908f56cf7804c9e0a50e318bd291
Stepanov Igor <stipyk1309@gmail.com>
2022-05-07 16:13:01 +0300

    lesson 50-51 Rest API get

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,48 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios"; //импортируем все что там экспортируется под общим названием axios
+import userPhoto from "../../assets/images/user.png"
 
 const Users = (props) => {
-
-	if (props.users.length === 0) 
-
-props.setUsers( [{
-	id: 1,
-	photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbyt0bSJhqucS4mE6t1y3EyusW7wKcGGgEnA&usqp=CAU",
-	followed: true,
-	fullName: "Varg",
-	status: "like a boss",
-	location: { city: "Oslo", country: "Norway" },
-  },
-  {
-	id: 2,
-	photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbyt0bSJhqucS4mE6t1y3EyusW7wKcGGgEnA&usqp=CAU",
-	followed: false,
-	fullName: "Iggy",
-	status: "junior-developer",
-	location: { city: "LA", country: "USA" },
-  },
-  {
-	id: 3,
-	photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbyt0bSJhqucS4mE6t1y3EyusW7wKcGGgEnA&usqp=CAU",
-	followed: false,
-	fullName: "Valera",
-	status: "че",
-	location: { city: "Petrozavodsk", country: "Russia" },
-  },])
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      }); //подключили дату с сервера
+  }
+  // props.setUsers( [{
+  // 	id: 1,
+  // 	photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbyt0bSJhqucS4mE6t1y3EyusW7wKcGGgEnA&usqp=CAU",
+  // 	followed: true,
+  // 	fullName: "Varg",
+  // 	status: "like a boss",
+  // 	location: { city: "Oslo", country: "Norway" },
+  //   },
+  //   {
+  // 	id: 2,
+  // 	photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbyt0bSJhqucS4mE6t1y3EyusW7wKcGGgEnA&usqp=CAU",
+  // 	followed: false,
+  // 	fullName: "Iggy",
+  // 	status: "junior-developer",
+  // 	location: { city: "LA", country: "USA" },
+  //   },
+  //   {
+  // 	id: 3,
+  // 	photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbyt0bSJhqucS4mE6t1y3EyusW7wKcGGgEnA&usqp=CAU",
+  // 	followed: false,
+  // 	fullName: "Valera",
+  // 	status: "че",
+  // 	location: { city: "Petrozavodsk", country: "Russia" },
+  //   },])
 
   return (
-    <div >
+    <div>
       {props.users.map((u) => (
-        <div >
+        <div>
           <span>
             <div>
-              <img className={styles.userPhoto} src={u.photoUrl} alt="avatar" />
+              <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt="avatar" />
             </div>
             <div>
               {u.followed ? (
@@ -60,12 +66,13 @@ props.setUsers( [{
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.city}</div>
+              {/* <div>{u.location.city}</div>
               <div>{u.location.country}</div>
+			  в дате нет таких сущностей поэтому пока закоментим */}
             </span>
           </span>
         </div>
